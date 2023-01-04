90c3775f1df53c82cfe11fbae489487482a92e98
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-01-04 23:08:13 +0530

    Create largest_two.cpp

---
+++ largest_two.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter Value:"<<endl;
+    cin>>a;
+    cout<<"a="<<a<<endl;
+    cin>>b;
+    cout<<"b="<<b<<endl;
+    if(a>b)
+    cout<<"a is greater";
+    else
+    cout<<"b is greater";
+    
+
+    return 0;
+}
