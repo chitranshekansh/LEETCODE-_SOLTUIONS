4fdda876b7eca20e760441257e65c05ce382b62e
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-01-04 23:03:25 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,16 @@
+//Program to print message or variable values and their sum
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=5,b=10;
+    cout<<"Hello this is my first program"<<endl;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"sum="<<a+b<<endl;
+    
+
+    return 0;
+}
