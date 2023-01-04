82851b951ac618cc9b9ffb8bb81e9ef1ea81d568
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-01-04 23:06:14 +0530

    Create Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,18 @@
+//Program to take input from the keyboard
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter Value:"<<endl;
+    cin>>a;
+    cout<<"a="<<a<<endl;
+    cin>>b;
+    cout<<"b="<<b<<endl;
+    cout<<"sub="<<a-b<<endl;
+    
+
+    return 0;
+}
